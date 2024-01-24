7473998a1403859d93e4ff54a3931b0f0bc6c4b4
clemphg <clementine@phung.fr>
2024-01-24 15:29:12 +0100

    commit de pause

--- crawler.py
+++ crawler.py
@@ -10,7 +10,10 @@ class Crawler():
 
     def __init__(self, start_url):
         self.start_url = start_url
-        self.visited_urls = []
+        self.to_visit = []
+        self.visited = []
+        self.max_urls = 50
+        self.crawl_delay = 5 # seconds
     
 
     def write_visited_urls(self, filename='crawled_webpages.txt', clear=True):
@@ -20,10 +23,10 @@ class Crawler():
 
         # write urls in filename
         with open(filename, 'a') as file:
-            for url in self.visited_urls:
+            for url in self.visited:
                 file.write(url + '\n')
     
-    def scan_links_in_page(pageurl):
+    def scan_links_in_page(self, pageurl):
 
         # initalise robot file parser (to only keep allowed urls)
         rp = RobotFileParser(os.path.join(pageurl,"robots.txt"))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from functions import scan_links_in_page, write_urls
+from crawler import Crawler
 
 def main():
     pass
@@ -7,10 +7,13 @@ def main():
 if __name__=="__main__":
 
     start_url = 'https://ensai.fr/'
-    urls = scan_links_in_page(start_url)
+
+    crawler = Crawler(start_url)
+
+    urls = crawler.scan_links_in_page(start_url)
 
     print(len(urls))
     # 220 with robots.txt
 
-    write_urls(urls, "test.txt")
+    crawler.write_visited_urls("test.txt")
 
