d8cdc470f3a1427759cbee3122265c27583e498e
clemphg <clementine@phung.fr>
2024-02-05 21:39:12 +0100

    modif .gitignore

--- ranking/test.py
+++ ranking/test.py
@@ -1,75 +1,73 @@
 import json
-import math
-from main import tokenize
+import re
 
-with open("title_pos_index.json") as f:
-    index_title = json.load(f)
+a = [[1],[2],[1,5,8],[4],[5],[6]]
+
+print(a[:-1])
+print(a[1:])
+
+print([min(e1)<min(e2) for e1, e2 in zip(a[:-1], a[1:])])
 
 
-def compute_bm25(query, index, documents, metadata, b, k1):
-    """Computes the bm25 score of each document for a specific query"""
+string = " Il fait beau "
 
+print(string.strip().split(' '))
+
+
+with open('ranking/documents.json') as f:
+    documents = json.load(f)
+    
+l1 = [4307, 11479, 11561]
+
+# l1=[1343, 3669, 13295]
+
+# l1=[3883, 3884, 5385, 5386]
+
+l1=[3979, 4912, 7842, 9130, 9691, 10755, 12731, 12981, 13007, 13151]
+
+docs = [doc for doc in documents if doc['id'] in l1]
+
+#for doc in docs:
+#    print(doc)
+
+#from nltk.corpus import stopwords
+#stopwords = set(stopwords.words('english'))
+#print(stopwords)
+
+def tokenize(text):
+    """Tokenizes a string (lowers+splits by whitespaces)"""
+    text = re.sub(r' +', ' ', text)
+    return text.lower().split(' ')
+
+def filter_docs(index:dict, query:str, filter:str) -> list[str]:
+    """Filters documents having all of the query's tokens"""
+    # maybe use title and content index for more results
+
+    # tokenize the query
     tokenized_query = tokenize(query)
 
-    document_scores = {}
+    # check if all query tokens are in index
+    # if not ignore these tokens
+    tokenized_query = [token for token in tokenized_query if token in index.keys()]
 
-    for doc in documents:
-        doc_id = doc['docId']
-        title = doc['title']
+    # print info about ignored tokens
 
-        bm25 = 0
+    if filter=='OR':
+        docs = []
         for token in tokenized_query:
-            if token in index.keys() and str(doc_id) in index[token].keys():
-                print('ok')
-
-                # compute inverse document frequency
-                idf = math.log(len(documents)/len(index[token].keys()))
-
-                # compute the frequency (count) of token in title of doc
-                freq_tok_in_doc = index[token][str(doc_id)]['count']
-
-                # fieldLen/avg(fieldLen)
-                field_len = len(tokenize(title))
-                avg_field_len = metadata['mean_nb_tokens_title']
-
-                # compute bm25
-                bm25 += idf*(freq_tok_in_doc*(k1+1))/(freq_tok_in_doc+k1*(1-b+b*field_len/avg_field_len))
-
-        document_scores[doc_id] = bm25
-
-    return document_scores
+            # only keep documents which contain at least one query token
+            docs = list(set(docs) | set(int(doc) for doc in index[token].keys()))
+    else: # default behaviour = 'AND'
+        docs = list(index[tokenized_query[0]].keys())
+        for token in tokenized_query[1:]:
+            # only keep documents which contain all the previous tokens
+            docs = [int(doc) for doc in docs if str(doc) in index[token].keys()]    
+    return docs
 
 
-documents = [
-    {
-        'docId': 1,
-        'title': "Des nouvelles très importantes sur la vie de nos amis les chiens"
-    },
-    {
-        'docId': 2,
-        'title': "J'ai de nouvelles chaussures vertes"
-    },
-    {
-        'docId': 3,
-        'title': "Les nouvelles vont vite dans cette ville traversée par des coulées vertes , très vertes"
-    },
-]
+index = {"hello": {'1':{}, '2':{}}, 
+         'world': {'3':{}, '2':{}},
+         '!': {'1':{}, '4':{}}}
 
-metadata = {
-    'mean_nb_tokens_title': sum([len(tokenize(doc['title'])) for doc in documents])/len(documents)
-}
-
-index_title = {
-    'des': {'1': {'count': 1}},
-    'nouvelles': {'1': {'count': 1}, '2': {'count': 1}, '3': {'count': 1}},
-    'vertes': {'2': {'count': 1}, '3': {'count': 3}}
-}
-
-
-query = 'nouvelles vertes'
-
-# params bm25
-b = 0.75
-k1 = 1.2
-
-print(compute_bm25(query, index_title, documents, metadata, b, k1))
+query = 'hello world'
+print(filter_docs(index, query, 'OR'))
