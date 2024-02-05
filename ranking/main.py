ac924ac18cbb50dbd4fdc7715ee199c79b4dfe01
clemphg <clementine@phung.fr>
2024-02-05 19:50:54 +0100

    discount in linear_ranking if token is stopword

--- ranking/main.py
+++ ranking/main.py
@@ -7,6 +7,8 @@ import json
 import math
 import argparse
 
+from nltk.corpus import stopwords
+
 def tokenize(text):
     """Tokenizes a string (lowers+splits by whitespaces)"""
     text = re.sub(r' +', ' ', text)
@@ -14,8 +16,8 @@ def tokenize(text):
 
 def get_stopwords(language: str) -> list[str]:
     """Stopwords list of a given language"""
-    stopwords = set(stopwords.words(language))
-    return stopwords
+    lstopwords = set(stopwords.words(language))
+    return lstopwords
 
 def filter_docs(index:dict, query:str, filter:str) -> list[str]:
     """Filters documents having all of the query's tokens"""
@@ -42,15 +44,17 @@ def filter_docs(index:dict, query:str, filter:str) -> list[str]:
             docs = [int(doc) for doc in docs if str(doc) in index[token].keys()]    
     return docs
 
-def linear_ranking(query, filtered_docs, index, weights):
+def linear_ranking(query, filtered_docs, index, weights, language):
     """Ranks documents based on a linear ranking score."""
 
-    # Tokenize the query
+    # tokenize the query
     query_tokens = query.lower().split()
 
-    # Initialize a dictionary to store document scores
+    # initialize a dictionary to store document scores
     document_scores = {}
 
+    # stopwords list
+    lstopwords = get_stopwords(language)
 
     # compute scores for each document
     for document in filtered_docs:
@@ -60,13 +64,14 @@ def linear_ranking(query, filtered_docs, index, weights):
         # Initialize score
         score = 0
 
-        # score 1: nb of query tokens in title
+        # score 1: nb of query tokens in title (stopwords don't count as much as non stopwords)
         tokenized_title = title.split()
-        num_q_tokens_in_title = len([tok for tok in list(set(query_tokens)) if tok in tokenized_title])
+        q_tokens_in_title = [tok for tok in list(set(query_tokens)) if tok in tokenized_title]
+        num_q_tokens_in_title = sum([weights['stopwords'] if tok in lstopwords else 1 for tok in q_tokens_in_title])
         score += weights['num_q_tokens_in_title']*num_q_tokens_in_title
 
         # score 2: nb token of query in title/nb of tokens in title$
-        num_tokens_in_title = len(tokenized_title)
+        num_tokens_in_title = sum([weights['stopwords'] if tok in lstopwords else 1 for tok in tokenized_title])
         score += weights['prop_tokens'] * num_q_tokens_in_title/num_tokens_in_title
 
         l_positions = []
@@ -76,8 +81,10 @@ def linear_ranking(query, filtered_docs, index, weights):
                 l_positions.append(positions)
 
                 # score 3: the closer tokens are to the beginning of a sentence, the bigger the weight
-                importance_feature = sum([1 / math.sqrt(pos+1) for pos in positions])
-                score += weights['position'] * importance_feature
+                # unless token is a stopword in which case we don't count it as much
+                discount = weights['stopwords'] if token in lstopwords else 1
+                pos_score = sum([discount / math.sqrt(pos+1) for pos in positions])
+                score += weights['position'] * pos_score
         
         # score 4: nb of ordered pairs of tokens
         score += weights['order']*sum([min(p1)<min(p2) for p1, p2 in zip(l_positions[:-1], l_positions[1:])])
@@ -126,7 +133,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("query",
-                        help="query for ranking.",
+                        help="query for ranking",
                         type=str)
     parser.add_argument("-f", "--filter",
                         default='AND',
@@ -148,6 +155,10 @@ def main():
     parser.add_argument("-r", "--results", 
                         default='results.json', 
                         help="filename for ranking results, default 'results.json'",
+                        type=str)
+    parser.add_argument("-l", "--language", 
+                        default='french', 
+                        help="language of documents, to compute stopwords list",
                         type=str)
     
     args = parser.parse_args()
@@ -173,12 +184,15 @@ def main():
     # selecting documents whos ids where kept through the filter
     filtered_docs = [doc for doc in documents if doc['id'] in filtered_docs_ids]
 
-    # ranking the documents
+    # setting weigths to compute the score in linear_ranking 
     weights = {"num_q_tokens_in_title": 1,
                "prop_tokens": 0.5,
                "position": 1.0,
-               "order": 2}
-    ranking = linear_ranking(query, filtered_docs, index_title, weights)
+               "order": 2,
+               "stopwords": 0.2}
+
+    # ranking the documents
+    ranking = linear_ranking(query, filtered_docs, index_title, weights, args.language)
 
     # extract title and url of ranked documents
     formated_ranking = format_ranking_results(documents, ranking)
