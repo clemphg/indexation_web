c4061f8338d95fac04258bfe5f64409f0830e004
clemphg <clementine@phung.fr>
2024-02-05 18:57:41 +0100

    clean up ranking lineaire

--- ranking/main.py
+++ ranking/main.py
@@ -12,7 +12,12 @@ def tokenize(text):
     text = re.sub(r' +', ' ', text)
     return text.lower().split(' ')
 
-def filter_docs(index, query) -> list[str]:
+def get_stopwords(language: str) -> list[str]:
+    """Stopwords list of a given language"""
+    stopwords = set(stopwords.words(language))
+    return stopwords
+
+def filter_docs(index:dict, query:str, filter:str) -> list[str]:
     """Filters documents having all of the query's tokens"""
     # maybe use title and content index for more results
 
@@ -25,11 +30,16 @@ def filter_docs(index, query) -> list[str]:
 
     # print info about ignored tokens
 
-    docs = list(index[tokenized_query[0]].keys())
-    for token in tokenized_query[1:]:
-        # only keep documents which contain all the previous tokens
-        docs = [int(doc) for doc in docs if str(doc) in index[token].keys()]
-    
+    if filter=='OR':
+        docs = []
+        for token in tokenized_query:
+            # only keep documents which contain at least one query token
+            docs = list(set(docs) | set(int(doc) for doc in index[token].keys()))
+    else: # default behaviour = 'AND'
+        docs = list(index[tokenized_query[0]].keys())
+        for token in tokenized_query[1:]:
+            # only keep documents which contain all the previous tokens
+            docs = [int(doc) for doc in docs if str(doc) in index[token].keys()]    
     return docs
 
 def linear_ranking(query, filtered_docs, index, weights):
@@ -42,7 +52,7 @@ def linear_ranking(query, filtered_docs, index, weights):
     document_scores = {}
 
 
-    # Calculate scores for each document
+    # compute scores for each document
     for document in filtered_docs:
         doc_id = document['id']
         title = document['title'].lower()
@@ -50,64 +60,35 @@ def linear_ranking(query, filtered_docs, index, weights):
         # Initialize score
         score = 0
 
-        # Feature 1: Number of tokens in title document
-        num_tokens_feature = len(title.split())
-        score += weights['num_tokens'] * 1/num_tokens_feature
+        # score 1: nb of query tokens in title
+        tokenized_title = title.split()
+        num_q_tokens_in_title = len([tok for tok in list(set(query_tokens)) if tok in tokenized_title])
+        score += weights['num_q_tokens_in_title']*num_q_tokens_in_title
 
-        # Feature 2: Importance based on token positions
-        for query_token in query_tokens:
-            if query_token in index and doc_id in index[query_token]:
-                positions = index[query_token][doc_id]['positions']
+        # score 2: nb token of query in title/nb of tokens in title$
+        num_tokens_in_title = len(tokenized_title)
+        score += weights['prop_tokens'] * num_q_tokens_in_title/num_tokens_in_title
+
+        l_positions = []
+        for token in query_tokens:
+            if token in index and doc_id in index[token]:
+                positions = index[token][doc_id]['positions']
+                l_positions.append(positions)
+
+                # score 3: the closer tokens are to the beginning of a sentence, the bigger the weight
                 importance_feature = sum([1 / math.sqrt(pos) for pos in positions])
-                score += weights['importance'] * importance_feature
+                score += weights['position'] * importance_feature
+        
+        # score 4: nb of ordered pairs of tokens
+        score += weights['order']*sum([min(p1)<min(p2) for p1, p2 in zip(l_positions[:-1], l_positions[1:])])
 
-        # Feature 3: Score on whether tokens are in the same order in the query and the title
-
-        # Save the score for the document
+        # store score for the document
         document_scores[doc_id] = score
 
-    tokenized_query = [token for token in tokenize(query) if token in index.keys()]
-    tokenized_query = list(set(tokenized_query))
-
-    work_token = {} # token: {docId: {position: [int], count: int}}
-
-    filtered_docs_ids = [doc['id'] for doc in filtered_docs]
-
-    for token in tokenized_query:
-        # keep token position and info for each doc
-        work_token[token] = {int(docId): value for docId, value in index[token].items()
-                            if int(docId) in filtered_docs_ids}
-
-    work_doc = {} # docId: {token1: count, token2: count}
-    for docId in filtered_docs_ids:
-        work_doc[docId] = {token: work_token[token][docId]for token in tokenized_query}
-
-    # avec une comprésention de dictionnaire
-    #work_doc = {docId: {token: work_token[token][docId]['count'] for token in tokenized_query} 
-    #            for docId in filtered_docs_ids}
-
-    # un score selon le compte de tokens (**2 pour donner plus d'importance à des docs où il y aurait plus d'occurences de ces tokens)
-    score_counts = {docId: sum([work_doc[docId][token]['count'] 
-                                for token in tokenized_query])**2 
-                    for docId in filtered_docs_ids}
-
-    d = {docId: [work_doc[docId][token]['positions'] for token in tokenized_query] for docId in filtered_docs_ids}
-
-    # un score ordonnés ou non (nombre de fois ou un token de la query est avant le suivant dans le titre )
-    # tokens query = ['a', 'b', 'c']
-
-    # returns sum([pos_a<pos_b, pos_b<posc]) pour chaque doc
-    # if title = 'a c b'
-    score_o = {docId: sum([min(p1)<min(p2) for p1, p2 in zip(d[docId][:-1], d[docId][1:])]) for docId in filtered_docs_ids}
-
-    document_scores = {docId: score+score_o[docId] for docId, score in document_scores.items()}
-
-    # Rank documents based on scores
+    # rank documents based on scores
     ranked_documents = dict(sorted(document_scores.items(), key=lambda x: x[1], reverse=True))
-
     ranks = {rank+1: docId for rank, docId in zip(list(range(len(filtered_docs))), ranked_documents.keys())}
 
-    # Return the ranked documents
     return ranks
 
 def format_ranking_results(documents, ranking) -> list[dict]:
@@ -147,6 +128,11 @@ def main():
     parser.add_argument("query",
                         help="query for ranking.",
                         type=str)
+    parser.add_argument("-f", "--filter",
+                        default='AND',
+                        help="filter for the documents according to the query.",
+                        type=str,
+                        choices=['AND', 'OR'])
     parser.add_argument("-it", "--index_title", 
                         default='title_pos_index.json', 
                         help="filename of title positional index, default 'title_pos_index.json'",
@@ -179,21 +165,26 @@ def main():
     query = args.query
 
     # filtering documents containing all of the query tokens 
-    filtered_docs_ids = filter_docs(index_title, query)
-    print(f"Number of documents kept by the filter: {len(filtered_docs_ids)}")
+    filtered_docs_ids = filter_docs(index_title, query, args.filter)
+    print(f"Number of documents kept by the filter    : {len(filtered_docs_ids)}")
+    print(f"Proportion of documents kept by the filter: {round(100*len(filtered_docs_ids)/len(documents),3)}%")
+
 
     # selecting documents whos ids where kept through the filter
     filtered_docs = [doc for doc in documents if doc['id'] in filtered_docs_ids]
 
     # ranking the documents
-    weights = {"num_tokens": 0.5, "importance": 1.0}
+    weights = {"num_q_tokens_in_title": 1,
+               "prop_tokens": 0.5,
+               "position": 1.0,
+               "order": 1}
     ranking = linear_ranking(query, filtered_docs, index_title, weights)
 
     # extract title and url of ranked documents
     formated_ranking = format_ranking_results(documents, ranking)
 
     # save results as a list of dictionnaries, each with keys [title, url]
-    save_json(formated_ranking, 'sample.json')
+    save_json(formated_ranking, args.results)
 
 
 if __name__=="__main__":
--- ranking/test2.py
+++ ranking/test2.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 a = [[1],[2],[1,5,8],[4],[5],[6]]
 
@@ -26,5 +27,47 @@ l1=[3979, 4912, 7842, 9130, 9691, 10755, 12731, 12981, 13007, 13151]
 
 docs = [doc for doc in documents if doc['id'] in l1]
 
-for doc in docs:
-    print(doc)
+#for doc in docs:
+#    print(doc)
+
+#from nltk.corpus import stopwords
+#stopwords = set(stopwords.words('english'))
+#print(stopwords)
+
+def tokenize(text):
+    """Tokenizes a string (lowers+splits by whitespaces)"""
+    text = re.sub(r' +', ' ', text)
+    return text.lower().split(' ')
+
+def filter_docs(index:dict, query:str, filter:str) -> list[str]:
+    """Filters documents having all of the query's tokens"""
+    # maybe use title and content index for more results
+
+    # tokenize the query
+    tokenized_query = tokenize(query)
+
+    # check if all query tokens are in index
+    # if not ignore these tokens
+    tokenized_query = [token for token in tokenized_query if token in index.keys()]
+
+    # print info about ignored tokens
+
+    if filter=='OR':
+        docs = []
+        for token in tokenized_query:
+            # only keep documents which contain at least one query token
+            docs = list(set(docs) | set(int(doc) for doc in index[token].keys()))
+    else: # default behaviour = 'AND'
+        docs = list(index[tokenized_query[0]].keys())
+        for token in tokenized_query[1:]:
+            # only keep documents which contain all the previous tokens
+            docs = [int(doc) for doc in docs if str(doc) in index[token].keys()]    
+    return docs
+
+
+index = {"hello": {'1':{}, '2':{}}, 
+         'world': {'3':{}, '2':{}},
+         '!': {'1':{}, '4':{}}}
+
+query = 'hello world'
+print(filter_docs(index, query, 'OR'))
